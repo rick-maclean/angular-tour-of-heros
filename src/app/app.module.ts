542e7de93273e581a3a96dcba7afffb1c7dd821c
Rick MacLean <rick_maclean@sil.org>
2018-02-16 15:58:15 -0600

    state of tutorial Feb 16 4pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 
@@ -39,9 +40,10 @@ import { AppRoutingModule } from './app-routing.module';
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    HeroSearchComponent
   ],
-  providers: [ HeroService, MessageService ],
+  providers: [ HeroService, MessageService, InMemoryDataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
